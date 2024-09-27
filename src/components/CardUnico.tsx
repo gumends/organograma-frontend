bdf2bef5b9b3f8cd5be232260528db3c6099ae62
gumends <gustavo08.mendes@gmail.com>
2024-09-27 15:01:19 -0300

    att

--- src/components/CardUnico.tsx
+++ src/components/CardUnico.tsx
@@ -4,13 +4,15 @@ interface CardUnicoProps {
     texto: string;
     bgcolor: string;
     color: string
+    descricao?: string
 }
 
 export default function CardUnico(props: CardUnicoProps) {
     return (
-        <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <Sheet sx={{ width: 200, height: 70, bgcolor: props.bgcolor, display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <Typography level="h4" sx={{ fontSize: "17px", color: props.color }}>{props.texto}</Typography>
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", pb: 2 }}>
+            <Sheet sx={{ width: 200, height: 70, bgcolor: props.bgcolor, display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", textAlign: "center" }}>
+                <Typography level="h4" sx={{ fontSize: "17px", color: props.color }}>{props.texto.toLocaleUpperCase()}</Typography>
+                <Typography level="body-sm" sx={{ color: props.color }}>{props.descricao}</Typography>
             </Sheet>
         </Box>
     );
